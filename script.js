d673eef3f9f5f05c25d3556aff50976e386735bf
TiagoDou <tdouradowork@gmail.com>
2024-05-26 09:49:18 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const numStars = 100;
+const starsContainer = document.querySelector('.stars');
+
+for (let i = 0; i < numStars; i++) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.top = `${Math.random() * 100}vh`;
+    star.style.left = `${Math.random() * 100}vw`;
+    starsContainer.appendChild(star);
+}
